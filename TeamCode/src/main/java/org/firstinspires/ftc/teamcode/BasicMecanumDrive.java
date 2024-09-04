f66a062651bcacad1d5fa472c1e860772ca71fb3
Amloo <amloomis88@gmail.com>
2024-09-04 17:31:34 -0400

    Getting ready for new season

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BasicMecanumDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BasicMecanumDrive.java
@@ -3,43 +3,31 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorEx;
 
 @TeleOp(name="Basic: Mecanum Drive", group="Linear Opmode")
 
 public class BasicMecanumDrive extends LinearOpMode {
-    private DcMotorEx leftDriveFront = null;
-    private DcMotorEx rightDriveFront = null;
-    private DcMotorEx leftDriveBack = null;
-    private DcMotorEx rightDriveBack = null;
+    private DcMotor leftDriveFront = null;
+    private DcMotor rightDriveFront = null;
+    private DcMotor leftDriveBack = null;
+    private DcMotor rightDriveBack = null;
 
     @Override
     public void runOpMode() {
-
         telemetry.addData("Status", "Initialized");
         telemetry.update();
 
-        leftDriveFront = hardwareMap.get(DcMotorEx.class, "left_drive_front");
-        rightDriveFront = hardwareMap.get(DcMotorEx.class, "right_drive_front");
-        leftDriveBack  = hardwareMap.get(DcMotorEx.class, "left_drive_back");
-        rightDriveBack = hardwareMap.get(DcMotorEx.class, "right_drive_back");
-
-        leftDriveFront.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        rightDriveFront.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        leftDriveBack.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        rightDriveBack.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-
-        leftDriveFront.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        rightDriveFront.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        leftDriveBack.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        rightDriveBack.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        leftDriveFront = hardwareMap.get(DcMotor.class, "frontLeft");
+        rightDriveFront = hardwareMap.get(DcMotor.class, "frontRight");
+        leftDriveBack  = hardwareMap.get(DcMotor.class, "backLeft");
+        rightDriveBack = hardwareMap.get(DcMotor.class, "backRight");
 
         leftDriveFront.setDirection(DcMotor.Direction.REVERSE);
         rightDriveFront.setDirection(DcMotor.Direction.REVERSE);
         leftDriveBack.setDirection(DcMotor.Direction.FORWARD);
         rightDriveBack.setDirection(DcMotor.Direction.FORWARD);
-        waitForStart();
 
+        waitForStart();
         while (opModeIsActive()) {
             drive();
         }
@@ -65,9 +53,9 @@ public class BasicMecanumDrive extends LinearOpMode {
         bl -= gamepad1.right_stick_x;
         br += gamepad1.right_stick_x;
 
-        leftDriveFront.setVelocity(fl * 1000);
-        rightDriveFront.setVelocity(fr * 1000);
-        leftDriveBack.setVelocity(bl * 1000);
-        rightDriveBack.setVelocity(br * 1000);
+        leftDriveFront.setPower(fl * 10);
+        rightDriveFront.setPower(fr * 10);
+        leftDriveBack.setPower(bl * 10);
+        rightDriveBack.setPower(br * 10);
     }
 }
